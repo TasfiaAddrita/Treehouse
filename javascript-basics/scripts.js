b2e3a1b09470f7989ef76ffce39ddaef74114b63
Tasfia Addrita <tasfia.addrita@gmail.com>
2015-02-10 19:33:07 -0500

    Created storymaker

--- javascript-basics/scripts.js
+++ javascript-basics/scripts.js
@@ -1,1 +1,4 @@
-alert("Oh hey you're back for more?");
+var noun = prompt("Please write an noun: ");
+var verb = prompt("Please write a verb: ");
+var adjective = prompt("Please write an adjective: ")
+document.write("Once there was a " + noun + " who " + verb + "  to make the world a " + adjective + " place.");
